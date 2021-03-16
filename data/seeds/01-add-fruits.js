254543c62004fc74e950675c6e551149aa311b67
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-03-16 21:45:31 +0100

    wip

--- data/seeds/01-add-fruits.js
+++ data/seeds/01-add-fruits.js
@@ -3,8 +3,8 @@ exports.seed = function (knex) {
   return knex('fruits').truncate()
     .then(function () {
       return knex('fruits').insert([
-        { name: 'pear', weight_oz: 2.5, delicious: 0, color: 'red' },
-        { name: 'banana', weight_oz: 1.5, delicious: 1 },
+        // { name: 'pear', weight_oz: 2.5, delicious: 0, color: 'red' },
+        // { name: 'banana', weight_oz: 1.5, delicious: 1 },
         { name: 'apple', weight_oz: 0.5 },
       ]);
     });
